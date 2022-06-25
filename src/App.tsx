ca612e855366eff5b616535e41165192d49c5c82
Rodrigo Moreira <62709202+rodrigomoreir@users.noreply.github.com>
2022-06-25 18:21:42 -0300

    chore: global configurations

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,8 @@
-import { gql, useQuery } from "@apollo/client"
-// import { useEffect } from "react"
-// import { client } from "./lib/apollo"
-
-const GET_LESSONS_QUERY = gql`
-  query {
-    lessons {
-      id
-      title
-
-      teacher {
-        name
-      }
-    }
-  }
-`
-
-interface Lesson {
-  id: string
-  title: string
-}
+import { Event } from "./pages/Event"
 
 function App() {
-
-  const { data } = useQuery<{ lessons: Lesson[] }>(GET_LESSONS_QUERY)
-
-  console.log(data)
-
-  // UMA FORMA DE SE FAZER REQUESTS COM GRAPHQL
-  // useEffect(() => {
-  //   client.query({
-  //     query: GET_LESSONS_QUERY
-  //   }).then(response => {
-  //     console.log(response.data)
-  //   })
-  // }, [])
-
   return (
-    <ul>
-      {data?.lessons.map(lesson => {
-        return <li>{lesson.title}</li>
-      })}
-    </ul>
+    <Event />
   )
 }
 
